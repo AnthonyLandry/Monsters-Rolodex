f2f488507e55814e803189c2f9d379f12c3902d4
Anthony <anthonylandry777@gmail.com>
2019-11-27 20:00:17 -0800

    Updates

---
+++ precache-manifest.087d5112ac6aed113932b835ff922822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171b8ed8a0029c25dda2964f32a1e8b3",
+    "url": "/AnthonyLandry/Monsters-Rolodex.git/index.html"
+  },
+  {
+    "revision": "6a675542a626388566c9",
+    "url": "/AnthonyLandry/Monsters-Rolodex.git/static/css/main.5b990a0f.chunk.css"
+  },
+  {
+    "revision": "22555f7726a5e4b4c19a",
+    "url": "/AnthonyLandry/Monsters-Rolodex.git/static/js/2.77688bae.chunk.js"
+  },
+  {
+    "revision": "6a675542a626388566c9",
+    "url": "/AnthonyLandry/Monsters-Rolodex.git/static/js/main.12d5db4d.chunk.js"
+  },
+  {
+    "revision": "c67d739799a77663bc84",
+    "url": "/AnthonyLandry/Monsters-Rolodex.git/static/js/runtime-main.4fda4e1b.js"
+  }
+]);
